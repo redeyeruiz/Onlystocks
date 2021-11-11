9e4a379396a946934578e6a2b5fc2ad33a6605fd
redeyeruiz <a01366792@itesm.mx>
2021-11-11 10:46:14 -0600

    sepparate views folder

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require ('express')
 const app = express()
 const port = process.env.PORT || 3030
+var path = require('path')
 
 app.set('view engine','ejs')
-app.use(express.static('public/views'))
-
+app.use(express.static('views'))
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port)
